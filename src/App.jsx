9af7896c0132e79f8fd7417f36d56b877be688ee
RishabMunot <rishabmunot.az@gmail.com>
2020-05-05 22:00:50 +0530

    Keeper Note part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from "react"
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "./notes";
 
-function App(){
-    return (
-        <div>
-            <Header/>
-            <Note/>
-            <Footer/>
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(element => (
+        <Note title={element.title} content={element.content} />
+      ))}
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
